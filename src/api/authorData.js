5c7055b7e9c399c7d771d123be3236d6cd2875d6
kpqm195 <logeswaran.murugesan@astrazeneca.com>
2018-05-31 12:26:16 +0530

    simple react routing without using any thirdparty pacakge

---
+++ src/api/authorData.js
@@ -0,0 +1,15 @@
+module.exports = {
+  authors:
+  [
+      {
+          id: 'cory-house',
+          firstName: 'Cory',
+          lastName: 'House'
+      },
+      {
+          id: 'lokesh-murukesh',
+          firstName: 'Lokesh',
+          lastName: 'Murukesh'
+      }
+  ]
+};
